42927e4025c118ef42f78093d31bbabc4c237fa6
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/PersonalInfoAddOn@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/PersonalInfoAddOn.pm
+++ lib/Foswiki/Contrib/PersonalInfoAddOn.pm
@@ -1,6 +1,7 @@
 package Foswiki::Contrib::PersonalInfoAddOn;
 
 use vars qw( $VERSION $RELEASE );
+
 # This should always be $Rev$ so that Foswiki can determine the
 # checked-in status of the plugin. It is used by the build automation tools, so
 # you should leave it alone.
